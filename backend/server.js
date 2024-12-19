eea701c1e96ae0b1a47df43dd4c52b3541a231a6
A-DILEEP <dileepalla2003@gmail.com>
2024-12-19 22:21:03 +0530

    static-pages

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,40 @@
-import express from "express"
-import mongoose from "mongoose"
-import dotenv from 'dotenv'
-import cors from "cors"
-import bodyParser from "body-parser"
-import taskRoutes from "../backend/routes/taskRoutes.js"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+import taskRoutes from "../backend/routes/taskRoutes.js";
+import path from "path"; 
 
 dotenv.config();
 
-const app=express();
+const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URL, {
 
-}).then(()=>{
-    console.log("MongoDB COnnected Successfully");
-}).catch((err)=>{
+mongoose
+  .connect(process.env.MONGO_URL, {})
+  .then(() => {
+    console.log("MongoDB Connected Successfully");
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
-app.use("/api/tasks",taskRoutes);
-const PORT=process.env.PORT;
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`);
-})
+
+app.use("/api/tasks", taskRoutes);
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000; 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
