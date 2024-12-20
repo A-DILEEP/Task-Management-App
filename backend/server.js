fadc32bcb27ae4acca4ec22a14ec3b8c65c42d12
A-DILEEP <dileepalla2003@gmail.com>
2024-12-21 00:20:48 +0530

    fixed cors policy error

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "https://task-management-app-six-ecru.vercel.app/", 
+    origin: "https://task-management-app-six-ecru.vercel.app", 
     methods: "GET,POST,PUT,DELETE",
   })
 );
